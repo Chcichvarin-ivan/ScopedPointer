5c4bc1dd7338d257b301636c6a709f0c5f20b50e
Ivan Chichvarin <ichichvarin@humanplus.ru>
2024-04-16 00:20:40 +0300

    [:featrue] Added pointer functional
    Added operation * and -> to make it act like a pointer

--- scoped_pointer.cpp
+++ scoped_pointer.cpp
@@ -2,11 +2,16 @@
  * @Author: Ivan Chichvarin ichichvarin@humanplus.ru
  * @Date: 2024-04-15 23:40:11
  * @LastEditors: Ivan Chichvarin ichichvarin@humanplus.ru
- * @LastEditTime: 2024-04-15 23:47:42
+ * @LastEditTime: 2024-04-16 00:18:53
  * @FilePath: /ScopedPointer/scoped_pointer.cpp
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include <cassert>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <string>
+ 
 
 // Умный указатель, удаляющий связанный объект при своём разрушении.
 // Параметр шаблона T задаёт тип объекта, на который ссылается указатель
@@ -46,6 +51,32 @@ public:
         T* ret_val = ptr_;
         ptr_ = nullptr;
         return ret_val;
+    }
+    
+    // Оператор приведения к типу bool позволяет узнать, ссылается ли умный указатель
+    // на какой-либо объект
+    explicit operator bool() const noexcept {
+        return (ptr_ != nullptr);
+    }
+
+    // Оператор разыменования возвращает ссылку на объект
+    // Выбрасывает исключение std::logic_error, если указатель нулевой
+    T& operator*() const {
+        if(ptr_){
+            return *ptr_;
+        }else{
+            throw std::logic_error("Pointer is null");
+        }
+    }
+
+    // Оператор -> должен возвращать указатель на объект
+    // Выбрасывает исключение std::logic_error, если указатель нулевой
+    T* operator->() const {
+        if(ptr_){
+            return ptr_;
+        }else{
+            throw std::logic_error("Pointer is null");
+        }
     }
 
 private:
@@ -94,5 +125,94 @@ int main() {
         assert(!is_deleted);
         delete raw_ptr;
         assert(is_deleted);
+    }
+     // Проверка работы оператора приведения к типу bool
+    {
+        // Для нулевого указателя приведение к типу bool возвращает false
+        const ScopedPtr<int> empty_ptr;
+        assert(!empty_ptr);
+
+        // Для ненулевого указателя приведение к типу bool возвращает true
+        const ScopedPtr<int> ptr_to_existing_object(new int(0));
+        assert(ptr_to_existing_object);
+
+        static_assert(noexcept(static_cast<bool>(ptr_to_existing_object)));
+    }
+
+    // Проверка работы оператора разыменования *
+    {
+        std::string* raw_ptr = new std::string("hello");
+        ScopedPtr<std::string> smart_ptr(raw_ptr);
+        // Ссылка, возвращаемая оператором разыменования, должна ссылаться на объект,
+        // на который указывает умный указатель
+        assert(&*smart_ptr == raw_ptr);
+
+        try {
+            ScopedPtr<int> empty_ptr;
+            // При попытке разыменовать пустой указатель должно быть выброшено
+            // исключение logic_error
+            *empty_ptr;
+            // Сюда попасть мы не должны
+            assert(false);
+        } catch (const std::logic_error&) {
+            // мы там, где нужно
+        } catch (...) {
+            // Других исключений выбрасываться не должно
+            assert(false);
+        }
+    }
+
+    // Проверка работы оператора ->
+    {
+        std::string* raw_ptr = new std::string("hello");
+        ScopedPtr<std::string> smart_ptr(raw_ptr);
+        // Доступ к членам класса через умный указатель должен быть аналогичен
+        // доступу через "сырой" указатель
+        assert(smart_ptr->data() == raw_ptr->data());
+
+        try {
+            ScopedPtr<std::string> empty_ptr;
+            // При попытке разыменовать пустой указатель должно быть выброшено
+            // исключение logic_error
+            empty_ptr->clear();
+            // Сюда попасть мы не должны
+            assert(false);
+        } catch (const std::logic_error&) {
+            // мы там, где нужно
+        } catch (...) {
+            // Других исключений выбрасываться не должно
+            assert(false);
+        }
+    }
+
+    // Пример использования
+    {
+        using namespace std;
+        // На этой структуре будет проверяться работа умного указателя
+        struct Object {
+            Object() {
+                std::cout << "Object is default constructed" << std::endl;
+            }
+            void DoSomething() {
+                std::cout << "Doing something" <<std::endl;
+            }
+            ~Object() {
+                std::cout << "Object is destroyed" << std::endl;
+            }
+        };
+
+        // Сконструированный по умолчанию указатель ссылается на nullptr
+        ScopedPtr<Object> empty_smart_ptr;
+        // Перегруженный оператор приведения к типу bool вернёт false для пустого указателя
+        assert(!empty_smart_ptr);
+
+        ScopedPtr<Object> smart_ptr(new Object());
+        // Перегруженный оператор bool вернёт true для указателя, ссылающегося на объект
+        assert(smart_ptr);
+
+        // Проверка оператора разыменования
+        (*smart_ptr).DoSomething();
+        // Проверка оператора доступа к членам класса
+        smart_ptr->DoSomething();
     }
 }
